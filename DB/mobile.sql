d717f3606663f40e46e25068024ae901bcd8ff99
caipd <827963585@qq.com>
2020-01-15 11:20:13 +0800

    添加业务员搜索用户

--- DB/mobile.sql
+++ DB/mobile.sql
@@ -1,153 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 4.8.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 2020-01-05 09:37:53
--- 服务器版本： 5.6.40
--- PHP Version: 7.1.16
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : 本机数据库
+ Source Server Type    : MySQL
+ Source Server Version : 50553
+ Source Host           : localhost:3306
+ Source Schema         : mobile
 
+ Target Server Type    : MySQL
+ Target Server Version : 50553
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 14/01/2020 21:36:45
+*/
 
---
--- Database: `mobile`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `number`
---
-
-CREATE TABLE `number` (
-  `number_id` int(10) UNSIGNED NOT NULL,
-  `number_number` varchar(45) DEFAULT NULL COMMENT '手机的号码',
-  `number_status` varchar(45) DEFAULT NULL COMMENT '号码的状态【停用、注销、使用、未使用】',
+-- ----------------------------
+-- Table structure for number
+-- ----------------------------
+DROP TABLE IF EXISTS `number`;
+CREATE TABLE `number`  (
+  `number_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `number_number` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '手机的号码',
+  `number_status` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '号码的状态【停用、注销、使用、未使用】',
   `user_id` int(11) DEFAULT NULL COMMENT '所属的用户ID',
-  `packagelist_id_list` varchar(2000) DEFAULT NULL COMMENT '选择的套餐列表'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `packagelist_id_list` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '选择的套餐列表',
+  PRIMARY KEY (`number_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for packagelist
+-- ----------------------------
+DROP TABLE IF EXISTS `packagelist`;
+CREATE TABLE `packagelist`  (
+  `package_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `package_monthRent` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '月租',
+  `package_minCharge` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `package_msgCharge` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `package_totalTraffic` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '流量总额',
+  PRIMARY KEY (`package_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
---
--- 表的结构 `packagelist`
---
+-- ----------------------------
+-- Table structure for rent
+-- ----------------------------
+DROP TABLE IF EXISTS `rent`;
+CREATE TABLE `rent`  (
+  `rent_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `rent_content` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '收费项名称',
+  `rent_money` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '费用',
+  PRIMARY KEY (`rent_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
-CREATE TABLE `packagelist` (
-  `package_id` int(10) UNSIGNED NOT NULL,
-  `package_monthRent` varchar(45) DEFAULT NULL COMMENT '月租',
-  `package_minCharge` varchar(45) DEFAULT NULL,
-  `package_msgCharge` varchar(45) DEFAULT NULL,
-  `package_totalTraffic` varchar(45) DEFAULT NULL COMMENT '流量总额'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '	',
+  `user_password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_role` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_permission` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_sex` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_addrs` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_status` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'admin', 'admin', 'admin', 'admin', 'admin', '男', 'admin', '正常');
+INSERT INTO `user` VALUES (2, 'sale', 'sale', 'sale', 'sale', 'sale', '男', 'sale', '正常');
+INSERT INTO `user` VALUES (3, '菜菜', '123456', '123456', 'user', 'user', '男', 'user地址', '正常');
+INSERT INTO `user` VALUES (4, 'test停用状态的用户', '12345678', '12345678', 'user', 'user', '女', '用户的test地址', '停用');
+INSERT INTO `user` VALUES (5, '123456789test注销用户', '123456789', '123456789', 'user', 'user', '男', 'test地址123456789', '注销');
 
---
--- 表的结构 `rent`
---
-
-CREATE TABLE `rent` (
-  `rent_id` int(10) UNSIGNED NOT NULL,
-  `rent_content` varchar(45) DEFAULT NULL COMMENT '收费项名称',
-  `rent_money` varchar(45) DEFAULT NULL COMMENT '费用'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `user`
---
-
-CREATE TABLE `user` (
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `user_number` varchar(255) DEFAULT NULL COMMENT '	',
-  `user_password` varchar(45) DEFAULT NULL,
-  `user_role` varchar(45) DEFAULT NULL,
-  `user_permission` varchar(45) DEFAULT NULL,
-  `user_sex` varchar(45) DEFAULT NULL,
-  `user_addrs` varchar(45) DEFAULT NULL,
-  `user_status` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 转存表中的数据 `user`
---
-
-INSERT INTO `user` (`user_id`, `user_name`, `user_number`, `user_password`, `user_role`, `user_permission`, `user_sex`, `user_addrs`, `user_status`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin', 'admin', '男', 'admin', '正常'),
-(2, 'sale', 'sale', 'sale', 'sale', 'sale', '男', 'sale', '正常'),
-(3, '菜菜', '123456', '123456', 'user', 'user', '男', 'user地址', '正常'),
-(4, 'test停用状态的用户', '12345678', '12345678', 'user', 'user', '女', '用户的test地址', '停用'),
-(5, '123456789test注销用户', '123456789', '123456789', 'user', 'user', '男', 'test地址123456789', '注销');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `number`
---
-ALTER TABLE `number`
-  ADD PRIMARY KEY (`number_id`);
-
---
--- Indexes for table `packagelist`
---
-ALTER TABLE `packagelist`
-  ADD PRIMARY KEY (`package_id`);
-
---
--- Indexes for table `rent`
---
-ALTER TABLE `rent`
-  ADD PRIMARY KEY (`rent_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `number`
---
-ALTER TABLE `number`
-  MODIFY `number_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `packagelist`
---
-ALTER TABLE `packagelist`
-  MODIFY `package_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `rent`
---
-ALTER TABLE `rent`
-  MODIFY `rent_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
